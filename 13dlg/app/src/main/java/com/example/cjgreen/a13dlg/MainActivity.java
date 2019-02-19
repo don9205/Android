d0bed266e2c466a29a1b4c6e45e2d98ec3bef9ec
Unknown <don9205@naver.com>
2019-02-19 12:05:38 +0900

    13번째 앱 확인창

---
+++ 13dlg/app/src/main/java/com/example/cjgreen/a13dlg/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.cjgreen.a13dlg;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
+                dlg.setTitle("내가 제목");
+                dlg.setMessage("메세지 내용입니다");
+                dlg.setIcon(R.mipmap.ic_launcher);
+                dlg.setNegativeButton("취소",null);
+                dlg.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, "확인 눌렀습니다.",Toast.LENGTH_SHORT).show();
+                    }
+                });
+                dlg.show();
+            }
+        });
+    }
+}
